a535a0697a761130bab67437a08e38e1b52e8358
axmdl1 <mr.akhmedali@bk.ru>
2025-01-01 18:46:04 +0500

    db connection

--- backend/pkg/db/db.go
+++ backend/pkg/db/db.go
@@ -1,1 +1,56 @@
 package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+)
+
+type Config struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func newDB(cfg Config) (*sql.DB, error) {
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port,
+		cfg.User, cfg.Password, cfg.DBName, cfg.SSLMode)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to open database: %w", err)
+	}
+
+	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		return nil, fmt.Errorf("Failed to connect database: %w", err)
+	}
+
+	defer log.Println("Successfully connected to database")
+	return db, nil
+}
+
+func LoadConfigFromEnv() Config {
+	return Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     getenvAsInt("DB_PORT", 5432),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   os.Getenv("DB_NAME"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+	}
+}
+
+func getenvAsInt(key string, defaultValue int) int {
+	if value, exists := os.LookupEnv(key); exists {
+		var intValue int
+		fmt.Sscanf(value, "%d", &intValue)
+		return intValue
+	}
+	return defaultValue
+}
