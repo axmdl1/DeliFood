d7bdbbf22812d379e50f7c8e8734020c76b68fd2
axmdl1 <mr.akhmedali@bk.ru>
2025-01-07 17:39:31 +0500

    index.html runned to the server

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,37 +1,79 @@
-package cmd
+package main
 
 import (
 	"DeliFood/backend/pkg/logger"
 	"DeliFood/backend/pkg/middleware"
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
+/*
+var templates = template.Must(template.ParseFiles("index.frontend"))
+
+	func HomePageHandler(w http.ResponseWriter, r *http.Request) {
+		templates.ExecuteTemplate(w, "index.frontend", nil)
+	}
+*/
 func main() {
+	_, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	// Initialize logger
-	log := logger.NewLogger()
+	logger := logger.NewLogger()
 
 	// Log application start
-	log.Info("Application started", map[string]interface{}{
+	logger.Info("Application started", map[string]interface{}{
 		"module": "main",
 		"status": "success",
 	})
 
 	// Create Rate Limiter middleware
-	rateLimiter := middleware.NewRateLimiter(1, 3, log) // 1 request per second, burst of 3
+	rateLimiter := middleware.NewRateLimiter(1, 3, logger) // 1 request per second, burst of 3
 
 	// Example HTTP handler
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
+		http.ServeFile(w, r, "frontend/index.html")
 	})
 
-	// Wrap the handler with rate limiting
-	http.Handle("/", rateLimiter.Limit(handler))
+	// Serve static files from the "frontend" directory
+	/*http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./frontend/index.frontend")
+	})*/
+
+	fs := http.FileServer(http.Dir("./frontend"))
+	http.Handle("/", fs)
+	http.Handle("/menu", rateLimiter.Limit(handler))
 
 	// Start HTTP server
-	log.Info("Starting server on :8080", nil)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Error("Server failed to start", map[string]interface{}{
-			"error": err.Error(),
-		})
+	server := &http.Server{
+		Addr: ":9078",
 	}
+
+	go func() {
+		log.Println("Listening on :9078")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Could not start server: %s\n", err)
+		}
+	}()
+
+	//Gracefully shutdown
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+	<-signalChan
+	log.Println("Shutting down the server!")
+
+	cancel()
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdownCancel()
+	err := server.Shutdown(shutdownCtx)
+	if err != nil {
+		logger.Error("Could not shut down the server!!!", map[string]interface{}{"Error:": err})
+	}
+
+	logger.Warn("Server gracefully stopped", map[string]interface{}{"Module": "main"})
 }
--- go.mod
+++ go.mod
@@ -5,8 +5,7 @@ go 1.23
 require github.com/sirupsen/logrus v1.9.3
 
 require (
-	github.com/lib/pq v1.10.9 // indirect
-	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/crypto v0.32.0
 	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/time v0.9.0
 )
